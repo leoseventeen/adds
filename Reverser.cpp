c212cc906fc99c795024a8307f3acf5b7a3f1cb8
leoseventeen <90679875+leoseventeen@users.noreply.github.com>
2023-08-14 19:17:39 +0930

    Reverser.cpp

--- Reverser.cpp
+++ Reverser.cpp
@@ -1,15 +1,15 @@
 #include "Reverser.h"
-
+#include <cmath>  
 int Reverser::reverseDigit(int value) {
-    if (value < 0) return -1;  // Invalid input
-    if (value < 10) return value;  // Base case
-    
+    if (value < 0) return -1; 
+    if (value < 10) return value;  
+
     return value % 10 * std::pow(10, (int)std::log10(value)) + reverseDigit(value / 10);
 }
 
 std::string Reverser::reverseString(std::string characters) {
-    if (characters.empty()) return "";  // Base case
-    if (characters == "ERROR") return "ERROR";  // Invalid input
+    if (characters.empty()) return "";  
+    if (characters == "ERROR") return "ERROR";  
     
     return characters.back() + reverseString(characters.substr(0, characters.size() - 1));
 }
