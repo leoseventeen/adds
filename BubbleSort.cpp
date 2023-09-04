904685fe10f4771d79b456b7e69096477a2f0370
leoseventeen <90679875+leoseventeen@users.noreply.github.com>
2023-09-04 14:27:48 +0930

    BubbleSort.cpp

--- BubbleSort.cpp
+++ BubbleSort.cpp
@@ -2,8 +2,8 @@
 
 std::vector<int> BubbleSort::sort(const std::vector<int>& list) {
     std::vector<int> result = list;
-    for(size_t i = 0; i < result.size(); i++) {
-        for(size_t j = 0; j < result.size() - i - 1; j++) {
+    for(std::size_t i = 0; i < result.size(); i++) {
+        for(std::size_t j = 0; j < result.size() - i - 1; j++) {
             if(result[j] > result[j + 1]) {
                 std::swap(result[j], result[j + 1]);
             }
