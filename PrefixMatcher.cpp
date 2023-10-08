6aac7d7921bea2aae2f05c926222b87be5db0601
leoseventeen <90679875+leoseventeen@users.noreply.github.com>
2023-10-08 10:39:07 +1030

    Update PrefixMatcher.cpp

--- PrefixMatcher.cpp
+++ PrefixMatcher.cpp
@@ -1,44 +1,36 @@
-struct BinaryTrieNode {
-    BinaryTrieNode* children[2];
-    int routerNumber;  // default -1 means no router at this node
-    BinaryTrieNode() {
-        children[0] = children[1] = nullptr;
-        routerNumber = -1;
-    }
-};
+#include "PrefixMatcher.h"
 
-class PrefixMatcher {
-private:
-    BinaryTrieNode* root;
+BinaryTrieNode::BinaryTrieNode() {
+    children[0] = children[1] = nullptr;
+    routerNumber = -1;
+}
 
-public:
-    PrefixMatcher() {
-        root = new BinaryTrieNode();
-    }
+PrefixMatcher::PrefixMatcher() {
+    root = new BinaryTrieNode();
+}
 
-    void insert(string address, int routerNumber) {
-        BinaryTrieNode* node = root;
-        for (char c : address) {
-            int index = c - '0';
-            if (!node->children[index]) {
-                node->children[index] = new BinaryTrieNode();
-            }
-            node = node->children[index];
+void PrefixMatcher::insert(string address, int routerNumber) {
+    BinaryTrieNode* node = root;
+    for (char c : address) {
+        int index = c - '0';
+        if (!node->children[index]) {
+            node->children[index] = new BinaryTrieNode();
         }
-        node->routerNumber = routerNumber;
+        node = node->children[index];
     }
+    node->routerNumber = routerNumber;
+}
 
-    int selectRouter(string networkAddress) {
-        BinaryTrieNode* node = root;
-        int matchedRouter = -1;  // Default: No matched router
-        for (char c : networkAddress) {
-            int index = c - '0';
-            if (!node->children[index]) break;
-            node = node->children[index];
-            if (node->routerNumber != -1) {
-                matchedRouter = node->routerNumber;
-            }
+int PrefixMatcher::selectRouter(string networkAddress) {
+    BinaryTrieNode* node = root;
+    int matchedRouter = -1;
+    for (char c : networkAddress) {
+        int index = c - '0';
+        if (!node->children[index]) break;
+        node = node->children[index];
+        if (node->routerNumber != -1) {
+            matchedRouter = node->routerNumber;
         }
-        return matchedRouter;
     }
-};
+    return matchedRouter;
+}
