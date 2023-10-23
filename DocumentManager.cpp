e0bd664e8ad8b5586330b548b2565144196e6015
leoseventeen <90679875+leoseventeen@users.noreply.github.com>
2023-10-23 11:14:51 +1030

    Update DocumentManager.cpp

--- DocumentManager.cpp
+++ DocumentManager.cpp
@@ -1,49 +1,39 @@
 #include "DocumentManager.h"
 
-Document::Document(int id, int license_limit) : id(id), license_limit(license_limit), current_borrowed(0) {}
-
-void DocumentManager::addDocument(std::string name, int id, int license_limit) {
-    doc_map[name] = Document(id, license_limit);
+void DocumentManager::addDocument(string name, int id, int license_limit) {
+    nameToIdMap[name] = id;
+    documents[id] = {name, id, license_limit};
+    borrowedCopies[id] = 0;
 }
 
 void DocumentManager::addPatron(int patronID) {
     patrons.insert(patronID);
 }
 
-int DocumentManager::search(std::string name) {
-    if(doc_map.find(name) != doc_map.end()) {
-        return doc_map[name].id;
+int DocumentManager::search(string name) {
+    if(nameToIdMap.find(name) != nameToIdMap.end()) {
+        return nameToIdMap[name];
     }
     return 0;
 }
 
 bool DocumentManager::borrowDocument(int docid, int patronID) {
-    // Check if the patron is valid
     if(patrons.find(patronID) == patrons.end()) {
+        // Patron not registered
         return false;
     }
-    
-    // Search for the document using its ID. This step assumes that document IDs are unique.
-    for(auto &entry : doc_map) {
-        if(entry.second.id == docid) {
-            if(entry.second.current_borrowed < entry.second.license_limit &&
-               doc_borrowers[docid].find(patronID) == doc_borrowers[docid].end()) {
-                entry.second.current_borrowed++;
-                doc_borrowers[docid].insert(patronID);
-                return true;
-            }
-            return false;
-        }
+    if(borrowedCopies[docid] >= documents[docid].license_limit) {
+        // License limit reached
+        return false;
     }
-    return false;
+    borrowedCopies[docid]++;
+    docToPatrons[docid].insert(patronID);
+    return true;
 }
 
 void DocumentManager::returnDocument(int docid, int patronID) {
-    for(auto &entry : doc_map) {
-        if(entry.second.id == docid && doc_borrowers[docid].find(patronID) != doc_borrowers[docid].end()) {
-            entry.second.current_borrowed--;
-            doc_borrowers[docid].erase(patronID);
-            return;
-        }
+    if(docToPatrons[docid].find(patronID) != docToPatrons[docid].end()) {
+        borrowedCopies[docid]--;
+        docToPatrons[docid].erase(patronID);
     }
 }
